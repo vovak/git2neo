d1a6d35365e5a63e339625aad376617cf1fcb8dc
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2017-03-23 11:56:35 +0100

    wip: test to assert linear performance of arbitrary methods

--- src/test/kotlin/org/sorcerers/git2neo/EmbeddedDbTest.kt
+++ src/test/kotlin/org/sorcerers/git2neo/EmbeddedDbTest.kt
@@ -18,25 +18,40 @@ fun createEmbeddedIndex(path: String): CommitIndex {
 }
 
 class EmbeddedDbTest: CommitIndexTestBase() {
-    fun generateInputs(limits: Pair<Int, Int>, steps: Int): List<Int> {
+    fun generateInputs(limits: Pair<Long, Long>, steps: Int): List<Long> {
         if (steps < 3) throw IllegalArgumentException("The minimum number of steps is 3")
         if (limits.first >= limits.second) {
             throw IllegalArgumentException("Lower limit should be less than upper")
         }
-        val result: MutableList<Int> = ArrayList()
+        val result: MutableList<Long> = ArrayList()
         val increment = 1.0 * (limits.second - limits.first) / (steps-1)
 
         result.add(limits.first)
-        (1..steps-2).mapTo(result) { Math.round(limits.first + it * increment).toInt() }
+        (1..steps-2).mapTo(result) { Math.round(limits.first + it * increment) }
         result.add(limits.second)
         return result
     }
 
     @Test
     fun testInputGeneration() {
-        val reference = listOf(1,4,7,10)
+        val reference = listOf(1L,4,7,10)
         val generated = generateInputs(Pair(1,10), 4)
-        Assert.assertArrayEquals(reference.toIntArray(), generated.toIntArray())
+        Assert.assertArrayEquals(reference.toLongArray(), generated.toLongArray())
+    }
+
+    fun measureTime(function: (Int) -> Any, input: Int): Long {
+        val now = System.currentTimeMillis()
+        function.invoke(input)
+        return System.currentTimeMillis() - now
+    }
+
+    fun testLinearity(inputs: List<Int>, times: List<Long>) {
+        val linearTimes = generateInputs(Pair(times.first(), times.last()), times.size)
+        for (i in 1..inputs.size - 2) {
+            val actualTime = times[i]
+            val idealTime = linearTimes[i]
+            //in quadratic case actual time is LOWER than ideal.
+        }
     }
 
     fun assertLinearPerformance(function: (Int) -> Any, limits: Pair<Int, Int>, steps: Int) {
