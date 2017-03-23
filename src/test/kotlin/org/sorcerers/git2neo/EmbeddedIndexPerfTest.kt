425b3da6db878973d29cde7e6773bccb854a3f98
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2017-03-23 17:14:44 +0100

    simple perf test for graph population

--- src/test/kotlin/org/sorcerers/git2neo/EmbeddedIndexPerfTest.kt
+++ src/test/kotlin/org/sorcerers/git2neo/EmbeddedIndexPerfTest.kt
@@ -1,13 +1,41 @@
 package org.sorcerers.git2neo
 
+import org.junit.Assert
+import org.junit.Test
 import java.io.File
+import java.util.*
 
 /**
  * Created by vovak on 3/23/17.
  */
-fun createEmbeddedIndex(path: String): CommitIndex {
-    val dir = File(path)
+val TEST_DB_PATH = "./tempdb"
+
+fun removeTestDb() {
+    File(TEST_DB_PATH).deleteRecursively()
+}
+
+fun createEmbeddedIndex(): CommitIndex {
+    removeTestDb()
+    val dir = File(TEST_DB_PATH)
     dir.mkdirs()
     dir.deleteOnExit()
     return CommitIndexFactory().loadOrCreateCommitIndex(dir)
+}
+
+class EmbeddedIndexPerfTest: TimeComplexityTestBase() {
+    fun createDbAndAddUnconnectedNodes(nodesCount: Int) {
+        val index = createEmbeddedIndex()
+        val commits: MutableList<Commit> = ArrayList()
+        for (i in 1..nodesCount) {
+            commits.add(createCommit("$i", null))
+        }
+        index.addAll(commits)
+        index.db.shutdown()
+        removeTestDb()
+    }
+
+    @Test
+    fun testUnconnectedNodesAddInLinearTime() {
+        Assert.assertTrue(isLinearPerformance({createDbAndAddUnconnectedNodes(it) }, Pair(1000,100000), 15))
+    }
 }
