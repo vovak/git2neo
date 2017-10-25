1e4f6070084c4e76384c67b3f463f43bdcff2021
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2017-10-26 01:42:31 +0200

    add more assertions to the low-level change connection structure test

--- src/test/kotlin/org/sorcerers/git2neo/ChangeConnectionStructureTest.kt
+++ src/test/kotlin/org/sorcerers/git2neo/ChangeConnectionStructureTest.kt
@@ -55,14 +55,16 @@ class ChangeConnectionStructureTest : CommitIndexTestBase() {
         commits.add(createCommit("4", "2", listOf(Triple(Action.MODIFIED, "a", null), Triple(Action.MODIFIED, "b", null))))
         commits.add(createCommit("5", "4", listOf(Triple(Action.MODIFIED, "a", null))))
         commits.add(createCommit("6", "3", listOf(Triple(Action.MODIFIED, "a", null))))
-        commits.add(createCommit("7", listOf("5", "6"), emptyList()))
+        commits.add(createCommit("7", listOf("5", "6"), listOf(Triple(Action.MODIFIED, "b", null))))
         commits.add(createCommit("8", "7", listOf(Triple(Action.MODIFIED, "a", null))))
         commits.add(createCommit("9", "8", listOf(Triple(Action.MODIFIED, "a", null), Triple(Action.MODIFIED, "b", null))))
 
         index.addAll(commits)
 
         Assert.assertEquals("Change node should only be connected to the nearest parents", 1, findFileChangeNodeParentConnections(index, "9", "a").size) // to change in node #8
-        Assert.assertEquals("Change node should only be connected to the nearest parents", 2, findFileChangeNodeParentConnections(index, "9", "b").size) // to changes in nodes #3 and #4
+        Assert.assertEquals("Change node should only be connected to the nearest parents", 1, findFileChangeNodeParentConnections(index, "9", "b").size) // to change in node #7
+        Assert.assertEquals("Change node should only be connected to the nearest parents", 2, findFileChangeNodeParentConnections(index, "7", "b").size) // to changes in nodes #3 and #4
+        Assert.assertEquals("Change node should only be connected to the nearest parents", 2, findFileChangeNodeParentConnections(index, "8", "a").size) // to changes in nodes #5 and #6
     }
 
 }
