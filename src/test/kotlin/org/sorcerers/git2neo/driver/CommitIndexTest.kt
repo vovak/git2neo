6673023e3670f96dacfdff95dca801df321ae59e
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2018-04-10 15:50:35 +0200

    add tests for file revision parent IDs retrieval

--- src/test/kotlin/org/sorcerers/git2neo/driver/CommitIndexTest.kt
+++ src/test/kotlin/org/sorcerers/git2neo/driver/CommitIndexTest.kt
@@ -5,6 +5,7 @@ import org.junit.Test
 import org.sorcerers.git2neo.model.Action
 import org.sorcerers.git2neo.model.Commit
 import org.sorcerers.git2neo.model.CommitId
+import org.sorcerers.git2neo.util.getFileRevisionId
 import java.util.*
 
 /**
@@ -74,6 +75,22 @@ class CommitIndexTest : CommitIndexTestBase() {
 
         val changesHistory = index.getChangesHistory(headCommitChange.id)
         Assert.assertEquals(2, changesHistory.items.size)
+    }
+
+    @Test
+    fun testParentFileRevisionWithTrivialChangesHistory() {
+        val index = getIndex()
+        index.add(createCommit("0", null, listOf(Triple(Action.CREATED, "a.txt", null))))
+        index.add(createCommit("1", "0", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        val headCommit = index.get(CommitId("1"))
+        Assert.assertNotNull(headCommit)
+        val headCommitChange = headCommit!!.changes.first()
+
+        val parentRevisions = headCommitChange.parentRevisions
+        Assert.assertNotNull(parentRevisions)
+        Assert.assertEquals(1, parentRevisions!!.size)
+
+        Assert.assertEquals(getFileRevisionId("0", "a.txt").stringId(), parentRevisions.first().id)
     }
 
     @Test
@@ -221,5 +238,46 @@ class CommitIndexTest : CommitIndexTestBase() {
         Assert.assertEquals(1, wholeHistories.size)
         val fileHistory = wholeHistories.first()
         Assert.assertEquals(13, fileHistory.items.size)
+    }
+
+    @Test
+    fun testFileRevisionParentIdsWithMergeAddedLater() {
+        val index = getIndex()
+        val commits: MutableList<Commit> = ArrayList()
+
+        commits.add(createCommit("0_left", null, listOf(Triple(Action.CREATED, "a.txt", null))))
+        commits.add(createCommit("1_left", "0_left", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("2_left", "1_left", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("3_left", "2_left", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("4_left", "3_left", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("5_left", "4_left", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+
+        commits.add(createCommit("0_right", null, listOf(Triple(Action.CREATED, "a.txt", null))))
+        commits.add(createCommit("1_right", "0_right", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("2_right", "1_right", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("3_right", "2_right", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("4_right", "3_right", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("5_right", "4_right", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+
+        commits.add(createCommit("head", "merge", listOf(Triple(Action.MODIFIED, "a.txt", null))))
+        commits.add(createCommit("merge", listOf("5_left", "5_right"), emptyList()))
+        index.addAll(commits)
+
+        val wholeHistories = index.getChangesHistoriesForCommit(CommitId("head"))
+        Assert.assertEquals(1, wholeHistories.size)
+        val fileHistory = wholeHistories.first()
+
+        val headCommitChange = fileHistory.items.find { it.id == getFileRevisionId("head", "a.txt") }
+        Assert.assertNotNull(headCommitChange)
+
+        val headCommitChangeParents = headCommitChange!!.parentRevisions
+        Assert.assertNotNull(headCommitChangeParents)
+
+        Assert.assertEquals(2, headCommitChangeParents!!.size)
+
+        val parentChangeIds = headCommitChangeParents.map { it.stringId() }
+
+        Assert.assertTrue(parentChangeIds.contains(getFileRevisionId("5_left", "a.txt").stringId()))
+        Assert.assertTrue(parentChangeIds.contains(getFileRevisionId("5_right", "a.txt").stringId()))
     }
 }
